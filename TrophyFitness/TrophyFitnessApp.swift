12048a4dc40d65fbd7bafe41749629591a69b1db
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-12-31 18:43:43 -0500

    Initial Commit

---
+++ TrophyFitness/TrophyFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrophyFitnessApp.swift
+//  TrophyFitness
+//
+//  Created by Oscar Lopez on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TrophyFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
